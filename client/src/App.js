2aaa11e4c44d51d8c48cbddc19ba386ca3233330
Thoshinny-cyber <thoshbala@gmail.com>
2023-12-12 15:24:11 +0530

    fiels changed

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
-import React from 'react';
-import FileUpload from './components/FileUpload';
-import './App.css';
-
-const App = () => (
-  <div className='container mt-4'>
-    <h4 className='display-4 text-center mb-4'>
-      <i className='fab fa-react' /> React File Upload
-    </h4>
-
-    <FileUpload />
-  </div>
-);
+import FormData from "form-data";
+import Axios from "axios";
+import { useState } from "react";
+function App() {
+  const [file, setFile] = useState(null);
+  const upload = (e) => {
+    e.preventDefault();
+    let formData = new FormData();
+    formData.append("screenshot", file);
+    Axios.post("http://localhost:4000/", formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }).then((res) => {
+      console.log("Success ", res);
+    });
+  };
+  return (
+    <div className="App">
+      <input
+        type="file"
+        name="screenshot"
+        onChange={(e) => {
+          setFile(e.target.files[0]);
+        }}
+      />
+      <button onClick={(e) => upload(e)}>Submit</button>
+    </div>
+  );
+}
 
 export default App;
